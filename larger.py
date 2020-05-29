4e5f6dddc5f06ab76eff00a442d113a04636b995
ArchanaSNadhan <asnathan@gitam.edu>
2020-05-29 08:56:30 +0530

    modified

--- larger.py
+++ larger.py
@@ -1,10 +1,13 @@
+def largest_number_by_two_times(nums):
+    return (2*nums)
 x=[]
 n=int(input('enter a number:'))
 for i in range(1,n+1):
     a=int(input('enter the element:'))
     x.append(a)
 x.sort()
-if(x[-1]<(2*x[-2])):
+y=largest_number_by_two_times(x[-2])
+if(x[-1]<y):
     print('False')
 else:
     print('True') 
