b7466a606369fcee1b6a0cea79444fa1af92e5e4
ArchanaSNadhan <asnathan@gitam.edu>
2020-05-29 09:46:51 +0530

    modified

--- mid.py
+++ mid.py
@@ -1,2 +1,4 @@
-list=[1,4,6,8,2,5,7]
-print('mid element is:',list[int(len(list)/2)])
+def middle_of_list(nums):
+    return nums[int(len(nums)/2)]
+x=[1,4,6,8,2,5,7]
+print('mid element is:',middle_of_list(x))
