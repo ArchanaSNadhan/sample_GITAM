53368e3d13d05b6c14f87953309c4ae283d54b5a
ArchanaSNadhan <asnathan@gitam.edu>
2020-06-07 17:47:17 +0530

    commit

--- larger.py
+++ larger.py
@@ -6,8 +6,10 @@ for i in range(1,n+1):
     a=int(input('enter the element:'))
     x.append(a)
 x.sort()
+print('The elements of the list:')
+print(x)
 y=largest_number_by_two_times(x[-2])
 if(x[-1]<y):
     print('False')
 else:
-    print('True') 
+    print('True')
